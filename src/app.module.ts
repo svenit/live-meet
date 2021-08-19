35604c5f8736697afde6b2dbadd33ddfc9b3698a
svenit <vylq@topcv.vn>
2021-08-19 18:48:35 +0700

    update client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
-import { Module } from '@nestjs/common';
-
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { AuthModule } from './api/auth/auth.module';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumner: MiddlewareConsumer) {
+    consumner
+      .apply(AuthMiddleware)
+      .exclude({
+        path: 'auth/login',
+        method: RequestMethod.POST,
+      })
+      .forRoutes('*');
+  }
+}
