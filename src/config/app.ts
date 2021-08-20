b5edeb9a34a89676a74eb6f816a1d570942199b3
svenit <vylq@topcv.vn>
2021-08-20 11:32:27 +0700

    connect socket

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,5 +2,4 @@ export default {
   debug: true,
   appSecret: 'aaf69dfe5bca43ef0106378ab3f07a2a',
   allowOrigins: ['http://localhost:8080'],
-  socketPort: 3001,
 };
--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,10 +1,13 @@
 export default {
-  cors: {
-    origin: ['http://localhost:8080'],
+  port: 3001,
+  options: {
+    cors: {
+      origin: ['http://localhost:8080'],
+    },
+    cookie: {
+      httpOnly: true,
+      path: '/',
+    },
+    maxHttpBufferSize: 1e6,
   },
-  cookie: {
-    httpOnly: true,
-    path: '/',
-  },
-  maxHttpBufferSize: 1e6,
 };
